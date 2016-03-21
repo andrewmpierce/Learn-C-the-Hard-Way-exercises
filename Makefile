a5b53efc98561f49db0f4719c373f7cc7868bd83
andrewmpierce <apierce1992@gmail.com>
2016-03-20 20:56:15 -0400

    added ex5

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 CFLAGS=-Wall -g
 
-all:
-	ex1
-	ex3
 
 clean:
 	rm -f ex1
 	rm -f ex3
 	rm -f ex4
+	rm -f ex5
---
+++ ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* this is a comment */
+
+int main(int argc, char *argv[])
+{
+
+	int distance = 100; 
+	// this is another way to make comments
+
+	printf("You are %d miles away \n", distance);	
+
+	return 0;
+}
